05d23a74b46827877d0afcd85216fc9937a5c8df
JYCruz <josh.cruz@neu.edu.ph>
2024-11-25 18:27:00 +0800

    Create NewsAgency.java

---
+++ NewsAgency.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewsAgency implements NewsSubject {
+
+    private String news;
+    private List<Subscriber> subscribers = new ArrayList<>();
+
+    public String getNews() {
+        return news;
+    }
+
+    public void setNews(String news) {
+        this.news = news;
+        notifySubscribers();
+    }
+
+    @Override
+    public void attach(Subscriber subscriber) {
+        subscribers.add(subscriber);
+    }
+
+    @Override
+    public void detach(Subscriber subscriber) {
+        subscribers.remove(subscriber);
+    }
+
+    @Override
+    public void notifySubscribers() {
+        for (Subscriber subscriber : subscribers) {
+            subscriber.updateNews(this);
+        }
+    }
+}
