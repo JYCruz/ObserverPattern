294391ce89a30b88084449e7a043249df430abd7
JYCruz <josh.cruz@neu.edu.ph>
2024-11-25 18:28:40 +0800

    Create Subscriber.java

---
+++ Subscriber.java
@@ -0,0 +1,17 @@
+public class Subscriber implements SubscriberObserver {
+
+    private String subscriberName;
+
+    public String getSubscriberName() {
+        return subscriberName;
+    }
+
+    public void setSubscriberName(String subscriberName) {
+        this.subscriberName = subscriberName;
+    }
+
+    @Override
+    public void updateNews(NewsAgency newsAgency) {
+        System.out.println(subscriberName + " received news: " + newsAgency.getNews());
+    }
+}
